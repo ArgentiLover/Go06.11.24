69f2f720c3f47c495f11b8e8bed2b4ce5ac8604b
ArgentiLover <mofischl00@gmail.com>
2024-11-06 14:38:48 +0300

    Сделал 1 задание

--- 1/main.go
+++ 1/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+	var year int
+	fmt.Print("Введите год: ")
+	fmt.Fscan(os.Stdin, &year)
 
-	fmt.Println("Haha")
+	if (year%400 == 0) || (year%4 != 0) {
+		fmt.Println("Не високосный")
+	} else {
+		fmt.Println("Високосный")
+	}
 }
